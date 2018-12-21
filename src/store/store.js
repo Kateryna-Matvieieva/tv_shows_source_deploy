0974679e0bb3fca608eb1d75f3d9b4ef6e8e6fe8
Ekaterina Matvyeyeva <ekaterina.matvyeyeva.com>
2018-12-21 18:16:08 +0200

    Create store

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,16 @@ import { rootReducer } from '../reducers/reducers';
 import thunk from "redux-thunk";
 
 const initialState = {
+    data: [],
+    imgs: [],
+    genres: [],
+    loading: true,
+    error: false,
+    filter: 'popular',
+    option: null,
+    countOfPages: 1000,
+    currentPage: 1,
+    
   }
   
 export const store = createStore(rootReducer, initialState,  applyMiddleware(thunk))
