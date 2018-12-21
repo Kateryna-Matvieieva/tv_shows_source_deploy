0828ea71347274344a2079f7395084679c30ed2b
Ekaterina Matvyeyeva <ekaterina.matvyeyeva.com>
2018-12-21 17:36:09 +0200

    Create constants

---
+++ src/actions/actions.js
@@ -0,0 +1,11 @@
+import {    
+    START_REQUEST,
+    COUNT_OF_PAGES, FILTER,
+    GET_ERROR,
+    GET_RESPONSE,
+    GET_SIMILLAR_TV_SHOWS,
+    GET_TRANSLATIONS_TV_SHOWS,
+    GET_RECOMENDATIONS_TV_SHOWS,
+    GET_GENRES
+} from '../constants/constants';
+
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,4 +1,13 @@
-import {} from '../actions/actions';
+import {    
+      START_REQUEST,
+      COUNT_OF_PAGES, FILTER,
+      GET_ERROR,
+      GET_RESPONSE,
+      GET_SIMILLAR_TV_SHOWS,
+      GET_TRANSLATIONS_TV_SHOWS,
+      GET_RECOMENDATIONS_TV_SHOWS,
+      GET_GENRES
+  } from '../actions/actions';
 
 export function rootReducer (state, action) {
         
