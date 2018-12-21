651ce5ebf2b4f65a20f6f7ab01c1245fe05d67f2
Ekaterina Matvyeyeva <ekaterina.matvyeyeva.com>
2018-12-21 17:49:27 +0200

    Create actions, edit constants

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,11 +1,95 @@
-import {    
+import {
     START_REQUEST,
-    COUNT_OF_PAGES, FILTER,
+    COUNT_OF_PAGES,
+    FILTER,
     GET_ERROR,
     GET_RESPONSE,
-    GET_SIMILLAR_TV_SHOWS,
-    GET_TRANSLATIONS_TV_SHOWS,
-    GET_RECOMENDATIONS_TV_SHOWS,
+    GET_OPTIONAL_DATA,
     GET_GENRES
 } from '../constants/constants';
 
+export const response = ({ results, page }) => ({
+    type: GET_RESPONSE,
+    payload: {
+        results,
+        page
+    }
+})
+export const pages = ({ total_pages }) => ({
+    type: COUNT_OF_PAGES,
+    payload: {
+        countOfPages: total_pages
+    }
+})
+export const genres = ({ genres }) => ({
+    type: GET_GENRES,
+    payload: genres
+
+})
+export const filter = (filter) => ({
+    type: FILTER,
+    payload: filter
+})
+export const optional= (index, page = 1) => ({
+    type: GET_OPTIONAL_DATA,
+})
+
+export const error = (err) => ({
+    type: GET_ERROR,
+    payload: err
+})
+export const isLoading = (loading) => ({
+    type: START_REQUEST,
+    payload: loading
+})
+// export function errorAfterFiveSeconds() {
+//   return (dispatch) => {
+//       setTimeout(() => {
+//           dispatch(error());
+//       }, 5000);
+//   };
+// }
+export function fetchGenres() {
+    return (dispatch) => {
+        fetch(`https://api.themoviedb.org/3/genre/tv/list?&api_key=696d475c5616f9c15214877fbdf5bd6e&language=en-US`)
+            .then(res => {
+                return res.json()
+            })
+            .then(res => {
+                dispatch(genres(res));
+            })
+            .catch(() => dispatch(error(true)));
+    };
+}
+
+let prevFilter = 'popular';
+let counter = 1;
+export function fetchTableData(type = prevFilter, prev, page = counter, next, query) {
+    let url;
+    if (prev && page > 1)
+        --page;
+    if (next && page)
+        ++page;
+    if (type !== prevFilter)
+        page = 1;
+    if (query)
+        url = `https://api.themoviedb.org/3/search?query=${query}&api_key=696d475c5616f9c15214877fbdf5bd6e&language=en-US`
+    return (dispatch) => {
+        dispatch(isLoading(true));
+        fetch(url || `https://api.themoviedb.org/3/tv/${type}?page=${ page }&api_key=696d475c5616f9c15214877fbdf5bd6e&language=en-US`)
+            .then(res => {
+                return res.json()
+            })
+            .then(res => {
+                if (type !== prevFilter) {
+                    dispatch(pages(res));
+                    dispatch(filter(res))
+                }
+                dispatch(response(res));
+                prevFilter = type;
+                counter = page;
+
+            })
+            .catch(() => dispatch(error(true)));
+    };
+}
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -3,9 +3,7 @@ import {
       COUNT_OF_PAGES, FILTER,
       GET_ERROR,
       GET_RESPONSE,
-      GET_SIMILLAR_TV_SHOWS,
-      GET_TRANSLATIONS_TV_SHOWS,
-      GET_RECOMENDATIONS_TV_SHOWS,
+      GET_OPTIONAL_DATA,
       GET_GENRES
   } from '../actions/actions';
 
