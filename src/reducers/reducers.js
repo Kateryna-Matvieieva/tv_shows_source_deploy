3e5b8593147bd55432d959e99c107d4cdcfeffdd
Ekaterina Matvyeyeva <ekaterina.matvyeyeva.com>
2018-12-21 17:59:58 +0200

    Create reducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -7,6 +7,56 @@ import {
       GET_GENRES
   } from '../actions/actions';
 
-export function rootReducer (state, action) {
+
+
+export default function rootReducer (state, action) {
+          switch(action.type) {
+            case GET_GENRES:
+              let genres = {};
+              action.payload.forEach(item => genres[item.id] = item.name)
+              return {
+                ...state,
+                genres
+              }
+            case START_REQUEST:
+              return {
+                ...state,
+                loading: action.payload,
+                error: null
+              };
+            case GET_RESPONSE:
+              let imgs = action.payload.results.map(item => {
+                  let obj = new Image ();
+                  obj.src = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/'+item.poster_path;
+                  return obj;
+              });
+              return {
+                ...state,
+                data: action.payload.results,
+                currentPage: action.payload.page,
+                imgs,
+                loading: false
+              };
+            case COUNT_OF_PAGES:
+              return {
+                ...state,
+                ...action.payload
+              };
+            case FILTER:
+              return {
+                ...state,
+                ...action.payload
+              };
+            case GET_ERROR:
+              return {
+                ...state,
+                loading: false,
+                error: action.payload,
+                data: []
+              };
         
-      }
+            default:
+              return state;
+          }
+        }
+  
