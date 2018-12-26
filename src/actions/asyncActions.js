28ee22b100bcbb94463ae60fa75db8a8f9847393
Ekaterina Matvyeyeva <ekaterina.matvyeyeva.com>
2018-12-26 11:03:16 +0200

    Resolved bugs in fetchTableDataDecorator in asyncActions

--- src/actions/asyncActions.js
+++ src/actions/asyncActions.js
@@ -22,17 +22,18 @@ let counter = 1;
 let prevQuery;
 function fetchTableDataDecorator(wrapped) {
     return function() {
-      let obj = Object.assign({page: counter, query: prevQuery}, arguments[0]);
       //if obj.filterType does not exist (obj.filterType || prevFilter) means that we use the same filter type and can go to any page
       // (obj.filterType || prevFilter) - if obj.filterType and prevFilter exist, return obj.filterType (- current filter type)
       //compare with prev filter type
       //if they are different, we want to see new filtered table from the first page
       // another case: exist current query that different from prev query
       //in this case we want to see new table of query data from the first page
-      if (((obj.filterType || prevFilter) !== prevFilter) || (obj.query !== obj.prevQuery)) {
+      let obj = arguments[0];
+      obj.page = obj.page ||  counter;
+      obj.query = obj.query ||  prevQuery;
+      if (((obj.filterType || prevFilter) !== prevFilter) || (obj.query !== prevQuery)) {
           obj.page = 1;
-          console.log('type, prevFilter',obj.filterType, prevFilter, obj.filterType || prevFilter)
-      }
+       }
       (obj.query && !obj.filterType) ?
           obj.url = `${src}/search/tv?query=${obj.query}&page=${obj.page}${key}` : obj.url = `${src}/tv/${obj.filterType?obj.filterType:prevFilter?prevFilter:'popular'}?page=${ obj.page }${key}`;
     
@@ -52,11 +53,14 @@ function fetchData({filterType, page, query, url}) {
                 dispatch(pages(data));
                 dispatch(filter(data));
             }
-            dispatch(loadData(data));
+           
             prevFilter = filterType;
             counter = page;
+            console.log(`1) page: ${page},filterType: ${filterType}, prevQuery: ${prevQuery}, query: ${query}`)
             filterType ? prevQuery = undefined: prevQuery = query;
             
+            console.log(`2) page: ${page},filterType: ${filterType}, prevQuery: ${prevQuery}, query: ${query}`)
+            dispatch(loadData(data));
         }
     };
 }
