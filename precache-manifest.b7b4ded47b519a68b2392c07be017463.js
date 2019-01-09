3f3e6c1bafe8e9f9a411da42bee6c70fc86d89c3
Ekaterina Matvyeyeva <ekaterina.matvyeyeva.com>
2019-01-09 19:22:14 +0200

    Updates

---
+++ precache-manifest.b7b4ded47b519a68b2392c07be017463.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "005b01a92a8a962f852a458344105e5e",
+    "url": "/tv_shows_source_deploy/static/media/fon.005b01a9.jpg"
+  },
+  {
+    "revision": "da97f68e6c03c2394f1d",
+    "url": "/tv_shows_source_deploy/static/js/runtime~main.da97f68e.js"
+  },
+  {
+    "revision": "0a71ebd1ab39d5e48fff",
+    "url": "/tv_shows_source_deploy/static/js/main.0a71ebd1.chunk.js"
+  },
+  {
+    "revision": "9336e4084fb511b8a095",
+    "url": "/tv_shows_source_deploy/static/js/1.9336e408.chunk.js"
+  },
+  {
+    "revision": "0a71ebd1ab39d5e48fff",
+    "url": "/tv_shows_source_deploy/static/css/main.0102e000.chunk.css"
+  },
+  {
+    "revision": "25d4d36baa3e9165e0ccd91751c09769",
+    "url": "/tv_shows_source_deploy/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/tv_shows_source_deploy/precache-manifest.b7b4ded47b519a68b2392c07be017463.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/tv_shows_source_deploy/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
